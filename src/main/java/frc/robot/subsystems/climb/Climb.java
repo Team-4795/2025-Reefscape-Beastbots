7baa7b914ce4a4be6157ac527cd2941aff2e013c
Ozzie Hassel <ozzieh@skiff.com>
2025-01-25 16:37:11 -0500

    Climb Mechanism Started - Nitya

---
+++ src/main/java/frc/robot/subsystems/climb/Climb.java
@@ -0,0 +1,52 @@
+package frc.robot.subsystems.climb;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.Logger;
+
+public class Climb extends SubsystemBase {
+  private ClimbIO io;
+  private final ClimbIOInputsAutoLogged inputs = new ClimbIOInputsAutoLogged();
+
+  private double velocity = 0.0;
+  private static Climb instance;
+
+  private Climb(ClimbIO io) {
+    this.io = io;
+    io.updateInputs(inputs);
+  }
+
+  public static Climb getInstance() {
+    return instance;
+  }
+
+  public static Climb init(ClimbIO io) {
+    if (instance == null) {
+      instance = new Climb(io);
+    }
+    return instance;
+  }
+
+  public void setVelocity(double v) {
+    this.velocity = v;
+  }
+
+  public double getVelocity() {
+    return inputs.velocityRPM;
+  }
+
+  public double getCurrent() {
+    return inputs.current;
+  }
+
+  public double getVoltage() {
+    return inputs.voltage;
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs("climb", inputs);
+    io.setClimbVoltage(MathUtil.clamp(velocity * 12, -12, 12));
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/climb/ClimbConstants.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems.climb;
+
+public final class ClimbConstants {
+  public static final int CanID = 7;
+  public static final double kP = 1;
+  public static final double kI = 1;
+  public static final double kD = 1;
+  public static final int currentLimit = 40;
+  public static final double gearing = 10;
+  public static final double moi = 10;
+}
