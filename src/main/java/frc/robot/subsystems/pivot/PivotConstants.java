03cdb683223d195d7dff314e862c7dcded655345
ColinH0 <poutine503@gmail.com>
2025-01-27 18:02:05 -0500

    started pivot

---
+++ src/main/java/frc/robot/subsystems/pivot/PivotConstants.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems.pivot;
+
+import java.util.concurrent.Flow.Publisher;
+
+public class PivotConstants{
+    public static int pivotMotorID = 0; //not right
+    public static int stallLimit = 30; //not right
+    public static int freeLimit = 20; //not right
+}
---
+++ src/main/java/frc/robot/subsystems/pivot/PivotIO.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems.pivot;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface PivotIO {
+    @AutoLog
+    public static class PivotIOInputs {
+        public double position = 0;
+        public double velocity = 0;
+        public double voltage = 0;
+    }
+    public default void setVoltage(double voltage) {};
+    public default void updateInputs(PivotIOInputs inputs) {};
+    }
