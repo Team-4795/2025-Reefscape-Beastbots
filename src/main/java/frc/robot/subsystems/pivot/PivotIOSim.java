f3e98901c92a6d6bda41517b13ab30da3302811a
ColinH0 <poutine503@gmail.com>
2025-02-03 17:06:16 -0500

    pivot aboutta sim

--- src/main/java/frc/robot/subsystems/pivot/PivotIOSim.java
+++ src/main/java/frc/robot/subsystems/pivot/PivotIOSim.java
@@ -2,12 +2,20 @@ package frc.robot.subsystems.pivot;
 
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
-import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 
 public class PivotIOSim implements PivotIO {
-  private DCMotorSim pivotMotorSim =
-      new DCMotorSim(
-          LinearSystemId.createDCMotorSystem(DCMotor.getNEO(1), 0.50, 0.1), DCMotor.getNEO(1), 1);
+  private SingleJointedArmSim pivotMotorSim =
+      new SingleJointedArmSim(
+          LinearSystemId.createSingleJointedArmSystem(DCMotor.getNEO(1), 2, 1),
+          DCMotor.getNEO(1),
+          1,
+          1,
+          0.5,
+          3,
+          false,
+          0.5,
+          0);
 
   @Override
   public void setVoltage(double voltage) {
@@ -16,8 +24,8 @@ public class PivotIOSim implements PivotIO {
 
   @Override
   public void updateInputs(PivotIOInputs inputs) {
-    inputs.voltage = pivotMotorSim.getInputVoltage();
-    inputs.velocity = pivotMotorSim.getAngularVelocityRPM();
-    inputs.position = pivotMotorSim.getAngularPositionRotations();
+    inputs.voltage = pivotMotorSim.getInput(1);
+    inputs.velocity = pivotMotorSim.getVelocityRadPerSec();
+    inputs.position = pivotMotorSim.getAngleRads();
   }
 }
