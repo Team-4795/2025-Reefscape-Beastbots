27d15682f6ddf60b51294ffcbeb37aaeafe3f655
dragonthebee <dragonthebee@gmail.com>
2025-02-17 18:18:34 -0500

    started pid

--- src/main/java/frc/robot/subsystems/pivot/Pivot.java
+++ src/main/java/frc/robot/subsystems/pivot/Pivot.java
@@ -1,10 +1,19 @@
 package frc.robot.subsystems.pivot;
 
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.littletonrobotics.junction.Logger;
 
 public class Pivot extends SubsystemBase {
   private PivotIOInputsAutoLogged inputs = new PivotIOInputsAutoLogged();
+  private ProfiledPIDController controller = new ProfiledPIDController(
+    PivotConstants.kP, 
+    PivotConstants.kI, 
+    PivotConstants.kD, 
+    new TrapezoidProfile.Constraints(PivotConstants.kS, PivotConstants.kA));
+  private SimpleMotorFeedforward feedForward = new SimpleMotorFeedforward(PivotConstants.kS, PivotConstants.kV);
   private PivotIO io;
   private static Pivot instance;
   private double voltage = 0;
--- src/main/java/frc/robot/subsystems/pivot/PivotConstants.java
+++ src/main/java/frc/robot/subsystems/pivot/PivotConstants.java
@@ -4,4 +4,11 @@ public class PivotConstants {
   public static int pivotMotorID = 0; // not right
   public static int stallLimit = 30; // not right
   public static int freeLimit = 20; // not right
+  
+  public static double kP = 0;
+  public static double kI = 0;
+  public static double kD = 0;
+  public static double kS = 0;
+  public static double kA = 0;
+  public static double kV = 0;
 }
