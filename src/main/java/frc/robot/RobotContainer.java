f6dd5a1378cbe270eaca30c3aae95e4a1755b4ea
Thomas Jin <83102001+Bob1272001@users.noreply.github.com>
2025-03-06 22:10:09 -0500

    beastbot stuff, idk what this is

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -114,7 +114,7 @@ public class RobotContainer {
     NamedCommands.registerCommand("zero", AutoCommands.intake(0));
     NamedCommands.registerCommand("intake", AutoCommands.intake(1.0 / 12.0));
     NamedCommands.registerCommand("pivot", AutoCommands.pivot(-1.0 / 12.0));
-
+    
     // Set up auto routines
     autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
     // Configure the button bindings
--- src/main/java/frc/robot/commands/AutoCommands.java
+++ src/main/java/frc/robot/commands/AutoCommands.java
@@ -4,12 +4,14 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.Intake.Intake;
 import frc.robot.subsystems.climb.Climb;
+import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.pivot.Pivot;
 
 public class AutoCommands {
   private static final Climb climb = Climb.getInstance();
   private static final Pivot pivot = Pivot.getInstance();
   private static final Intake intake = Intake.getInstance();
+  private static final Drive drive = Drive.getInstance();
 
   public static Command intake(double speed) {
 
@@ -23,5 +25,4 @@ public class AutoCommands {
        Commands.waitSeconds(5),
        Commands.run(() -> pivot.setVoltage(0)));
   }
-
 }
